c7aa3b96e913e4f01c71097676eb87938fdd3c61
adferrinm <62492197+adferrinm@users.noreply.github.com>
2023-07-31 06:31:52 -0400

    Add deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yannerisfr.github.io',
-  base: '/yannerisfr',
+  base: '/yanne-cv',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
